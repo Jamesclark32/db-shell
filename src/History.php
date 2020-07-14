037bb2271a7ba872a6962ad3b8ffdd7dfde3ad5e
James Clark <jamesclark@mbp.local>
2020-07-14 05:46:46 -0500

    prevent duplicate history entries. add history array as readline_list_history() seems borked

--- src/History.php
+++ src/History.php
@@ -6,28 +6,42 @@ use Illuminate\Support\Arr;
 
 class History
 {
+    protected array $history = [];
+
     public function loadHistory(): void
     {
         $history = file($this->getHistoryFilePath());
 
         foreach ($history as $query) {
             readline_add_history(str_replace('\040', ' ', $query));
+            $this->history[] = str_replace('\040', ' ', $query);
         }
     }
 
     public function addQueryToHistoryIfShould(string $query): bool
     {
-        if (false === stripos($query, 'password')) {
-            readline_add_history($query);
-            file_put_contents($this->getHistoryFilePath(), PHP_EOL.trim($query), FILE_APPEND);
-            return true;
+        if (stripos($query, 'password')) {
+            return false;
         }
-        return false;
+
+        if ($this->matchesLastHistoryEntry($query)) {
+            return false;
+        }
+
+        readline_add_history($query);
+        $this->history[] = $query;
+        file_put_contents($this->getHistoryFilePath(), PHP_EOL . trim($query), FILE_APPEND);
+        return true;
     }
 
     protected function getHistoryFilePath(): string
     {
         $baseDir = Arr::get($_SERVER, 'HOME');
         return $baseDir . '/.mysql_history';
+    }
+
+    protected function matchesLastHistoryEntry(string $query): bool
+    {
+        return $this->history[count($this->history) - 1] === $query;
     }
 }
